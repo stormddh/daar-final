f6956a268b40285b238c56ac4e8211f7b0c8091b
carlo <segatcarlo95@gmail.com>
2022-02-05 03:34:49 +0100

    2 jacard graphs: 1 based on content & one on subjects

--- recommender/generate_graph.js
+++ recommender/generate_graph.js
@@ -3,35 +3,63 @@ const {extractWordSet} = require("./_recommender");
 const fs = require('fs');
 const folder = '../data/recommender/'
 
-
 // change with full doc list/api call/ db call whatever we'll have
 let doc_paths = fs.readdirSync('../data/')
 doc_paths = doc_paths
     .filter(p => p.endsWith('json'))
     .map(p => '../data/' + p)
 
+console.log("finished preprocessing file names")
+
 doc_wordset_mapping = {}
 
-for(let i = 0; i < doc_paths.length; i++){
-    const docId = doc_paths[i].split('.json')[0].split(/\\*\//)[doc_paths[i].split('.json')[0].split(/\\*\//).length - 1]
-    doc_wordset_mapping[docId] = extractWordSet(doc_paths[i])
-}
+function generateContentBasedGraph(doc_paths){
+    for(let i = 0; i < doc_paths.length; i++){
+        const docId = doc_paths[i].split('.json')[0].split(/\\*\//)[doc_paths[i].split('.json')[0].split(/\\*\//).length - 1]
+        doc_wordset_mapping[docId] = extractWordSet(doc_paths[i])
+    }
 
-GEOMETRIC_GRAPH_THRESHOLD = 0.33 // tweak this if needd
+    console.log("finished building word-sets for books")
 
-adjacendy_list = {}
+    GEOMETRIC_GRAPH_THRESHOLD = 0.40 // tweak this if needd
 
-for(let a in doc_wordset_mapping){
-    adjacendy_list[a] = []
-    for(let b in doc_wordset_mapping){
-        if(a === b){ continue; }
-        const pairScore = getJacardScore(doc_wordset_mapping[a], doc_wordset_mapping[b])
-        if(pairScore >= GEOMETRIC_GRAPH_THRESHOLD){
-            adjacendy_list[a] = [...adjacendy_list[a], b]
+    adjacency_list = {}
+
+    let keys = Object.keys(doc_wordset_mapping);
+    for(let i_out = 0; i_out < keys.length; i_out++){
+        adjacency_list[keys[i_out]] = []
+        for(let i_in = i_out + 1; i_in < keys.length; i_in++){
+            const pairScore = getJacardScore(doc_wordset_mapping[keys[i_out]], doc_wordset_mapping[keys[i_in]])
+            if(pairScore >= GEOMETRIC_GRAPH_THRESHOLD){
+                adjacency_list[keys[i_out]] = [...adjacency_list[keys[i_out]], keys[i_in]]
+            }
         }
     }
+    fs.writeFileSync(folder + 'jaccard_graph_content.json', JSON.stringify(adjacency_list), err => console.log("error writin the graph file"))
 }
 
-fs.writeFileSync(folder + 'jaccard_graph.json', JSON.stringify(adjacendy_list), err => console.log("error writin the graph file"))
+function generateContentSubjectGraph(doc_paths){
+    for(let i = 0; i < doc_paths.length; i++){
+        const docId = doc_paths[i].split('.json')[0].split(/\\*\//)[doc_paths[i].split('.json')[0].split(/\\*\//).length - 1]
+        doc_wordset_mapping[docId] = extractWordSet(doc_paths[i], true)
+    }
 
-console.log('saved the Jacard graph as an adjecency list in ' + folder)
+    GEOMETRIC_GRAPH_THRESHOLD = 0.5 // tweak this if needd
+
+    adjacency_list = {}
+
+    let keys = Object.keys(doc_wordset_mapping);
+    for(let i_out = 0; i_out < keys.length; i_out++){
+        adjacency_list[keys[i_out]] = []
+        for(let i_in = i_out + 1; i_in < keys.length; i_in++){
+            const pairScore = getJacardScore(doc_wordset_mapping[keys[i_out]], doc_wordset_mapping[keys[i_in]])
+            if(pairScore >= GEOMETRIC_GRAPH_THRESHOLD){
+                adjacency_list[keys[i_out]] = [...adjacency_list[keys[i_out]], keys[i_in]]
+            }
+        }
+    }
+    fs.writeFileSync(folder + 'jaccard_graph_subject.json', JSON.stringify(adjacency_list), err => console.log("error writin the graph file"))
+}
+
+generateContentBasedGraph(doc_paths)
+generateContentSubjectGraph(doc_paths)
--- recommender/get_recommendations.js
+++ recommender/get_recommendations.js
@@ -1,10 +1,21 @@
 const fs = require('fs');
 
-function getRecommendations(bookId){
+function getRecommendationsContent(bookId){
     /**
      * Given a bookId it return the book IDs of those book that are recommended
+     * based on content matching
      * @type {string}
      */
-    const rawFile = fs.readFileSync('../data/recommender/jaccard_graph.json', 'utf8')
+    const rawFile = fs.readFileSync('../data/recommender/jaccard_graph_content.json', 'utf8')
+    return JSON.parse(rawFile)['' + bookId + '']
+}
+
+function getRecommendationsSubject(bookId){
+    /**
+     * Given a bookId it return the book IDs of those book that are recommended
+     * based on subject matching
+     * @type {string}
+     */
+    const rawFile = fs.readFileSync('../data/recommender/jaccard_graph_subject.json', 'utf8')
     return JSON.parse(rawFile)['' + bookId + '']
 }
